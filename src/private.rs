e138d1e6c47e901ab4ec7e3b1dbd1ec99234c2a9
William Venner <william@venner.io>
2022-08-26 15:10:20 +0100

    Get rid of thread-local redaction buffer as it only provided a 10% performance boost

--- src/private.rs
+++ src/private.rs
@@ -1,87 +1,18 @@
-use std::fmt::{Debug, Write};
+use std::fmt::Debug;
 
-mod redact_buffer {
-    //! We use a thread-local buffer to avoid frequent reallocations.
-    //!
-    //! AFAIK [`std::fmt::Formatter`] and friends have an internal buffer too, but it's not exposed, so we can't use it.
-
-    use std::{
-        cell::{Ref, RefCell},
-        ops::Deref,
-        rc::Rc,
-    };
-
-    /// The maximum size of the buffer's extra capacity.
-    ///
-    /// The buffer can grow larger than this value, but once used, it will be shrunk back to this size.
-    const MAX_BUFFER_SIZE: usize = 1024;
-
-    thread_local! {
-        static REDACT_BUFFER: Rc<RefCell<RedactBuffers>> = Rc::new(RefCell::new(RedactBuffers {
-            debug_buffer: String::with_capacity(MAX_BUFFER_SIZE),
-            redacted_buffer: String::with_capacity(MAX_BUFFER_SIZE),
-        }));
-    }
-
-    #[derive(Clone)]
-    pub struct RedactBuffers {
-        pub debug_buffer: String,
-        pub redacted_buffer: String,
-    }
-    impl RedactBuffers {
-        fn clear(&mut self) {
-            self.debug_buffer.clear();
-            self.debug_buffer.shrink_to(MAX_BUFFER_SIZE);
-            self.redacted_buffer.clear();
-            self.redacted_buffer.shrink_to(MAX_BUFFER_SIZE);
-        }
-    }
-
-    pub struct RedactBufferRef(Rc<RefCell<RedactBuffers>>);
-    impl RedactBufferRef {
-        // HACK! This allows us to put a RedactBufferRef OR &str in place of a &str.
-        pub fn as_ref(&self) -> Ref<'_, str> {
-            Ref::map(self.borrow(), |inner| inner.redacted_buffer.as_str())
-        }
-    }
-    impl Deref for RedactBufferRef {
-        type Target = RefCell<RedactBuffers>;
-
-        #[inline(always)]
-        fn deref(&self) -> &Self::Target {
-            self.0.deref()
-        }
-    }
-    impl Drop for RedactBufferRef {
-        fn drop(&mut self) {
-            if let Some(cell) = Rc::get_mut(&mut self.0) {
-                cell.get_mut().clear();
-            } else {
-                // It'll get cleared by the other holder
-            }
-        }
-    }
-    impl std::fmt::Debug for RedactBufferRef {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            f.write_str(self.borrow().redacted_buffer.as_str())
-        }
-    }
-
-    #[inline]
-    pub fn redact_buffer<F>(op: F) -> Result<RedactBufferRef, std::fmt::Error>
-    where
-        F: FnOnce(&mut RedactBuffers) -> Result<(), std::fmt::Error>,
-    {
-        let mut bufs = RedactBufferRef(REDACT_BUFFER.with(|bufs| bufs.clone()));
-        {
-            let bufs = Rc::make_mut(&mut bufs.0).get_mut();
-            bufs.clear();
-            op(bufs)?;
-        }
-        Ok(bufs)
+#[repr(transparent)]
+pub struct DisplayDebug(String);
+impl std::fmt::Debug for DisplayDebug {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.0.as_str())
     }
 }
-use redact_buffer::{redact_buffer, RedactBufferRef, RedactBuffers};
+impl AsRef<str> for DisplayDebug {
+    #[inline(always)]
+    fn as_ref(&self) -> &str {
+        self.0.as_str()
+    }
+}
 
 pub struct RedactFlags {
     /// Sourced from [`std::fmt::Formatter::alternate`]
@@ -124,14 +55,14 @@ impl RedactFlags {
     /// Maximum number of characters to expose at the beginning and end of a partial redact.
     const MAX_PARTIAL_EXPOSE: usize = 3;
 
-    fn redact_partial(&self, str: &str, redacted_buffer: &mut String) {
+    fn redact_partial(&self, str: &str, redacted: &mut String) {
         let count = str.chars().filter(|char| char.is_alphanumeric()).count();
         if count < Self::MIN_PARTIAL_CHARS {
             for char in str.chars() {
                 if char.is_alphanumeric() {
-                    redacted_buffer.push(self.redact_char);
+                    redacted.push(self.redact_char);
                 } else {
-                    redacted_buffer.push(char);
+                    redacted.push(char);
                 }
             }
         } else {
@@ -144,81 +75,78 @@ impl RedactFlags {
                 if char.is_alphanumeric() {
                     if prefix_gas > 0 {
                         prefix_gas -= 1;
-                        redacted_buffer.push(char);
+                        redacted.push(char);
                     } else if middle_gas > 0 {
                         middle_gas -= 1;
-                        redacted_buffer.push(self.redact_char);
+                        redacted.push(self.redact_char);
                     } else {
-                        redacted_buffer.push(char);
+                        redacted.push(char);
                     }
                 } else {
-                    redacted_buffer.push(char);
+                    redacted.push(char);
                 }
             }
         }
     }
 
-    fn redact_full(&self, str: &str, redacted_buffer: &mut String) {
+    fn redact_full(&self, str: &str, redacted: &mut String) {
         for char in str.chars() {
             if char.is_whitespace() || !char.is_alphanumeric() {
-                redacted_buffer.push(char);
+                redacted.push(char);
             } else {
-                redacted_buffer.push(self.redact_char);
+                redacted.push(self.redact_char);
             }
         }
     }
 
-    fn redact_fixed(&self, width: usize, redacted_buffer: &mut String) {
-        redacted_buffer.reserve_exact(width);
+    fn redact_fixed(&self, width: usize, redacted: &mut String) {
+        redacted.reserve_exact(width);
         for _ in 0..width {
-            redacted_buffer.push(self.redact_char);
+            redacted.push(self.redact_char);
         }
     }
 }
 
-pub fn redact(this: &dyn Debug, redact: RedactFlags) -> Result<RedactBufferRef, std::fmt::Error> {
-    redact_buffer(|buffers| {
-        let RedactBuffers {
-            debug_buffer,
-            redacted_buffer,
-        } = &mut *buffers;
+pub fn redact(this: &dyn Debug, redact: RedactFlags) -> Result<DisplayDebug, std::fmt::Error> {
+    let mut redacted = String::new();
 
-        if redact.fixed > 0 {
-            redact.redact_fixed(redact.fixed as usize, redacted_buffer);
-            return Ok(());
-        }
+    if redact.fixed > 0 {
+        redact.redact_fixed(redact.fixed as usize, &mut redacted);
+        return Ok(DisplayDebug(redacted));
+    }
 
-        if redact.debug_alternate {
-            write!(debug_buffer, "{:#?}", this)?;
+    let debug_formatted = if redact.debug_alternate {
+        format!("{:#?}", this)
+    } else {
+        format!("{:?}", this)
+    };
+
+    redacted.reserve(debug_formatted.len());
+
+    // Specialize for Option<T>
+    if redact.is_option {
+        if debug_formatted == "None" {
+            // We don't need to do any redacting
+            // https://prima.slack.com/archives/C03URH9N43U/p1661423554871499
+        } else if let Some(inner) = debug_formatted
+            .strip_prefix("Some(")
+            .and_then(|inner| inner.strip_suffix(')'))
+        {
+            redacted.push_str("Some(");
+            redact.redact_partial(inner, &mut redacted);
+            redacted.push(')');
         } else {
-            write!(debug_buffer, "{:?}", this)?;
+            // This should never happen, but just in case...
+            redact.redact_full(&debug_formatted, &mut redacted);
         }
+        return Ok(DisplayDebug(redacted));
+    }
 
-        // Specialize for Option<T>
-        if redact.is_option {
-            if debug_buffer == "None" {
-                // We don't need to do any redacting
-                // https://prima.slack.com/archives/C03URH9N43U/p1661423554871499
-            } else if let Some(inner) = debug_buffer
-                .strip_prefix("Some(")
-                .and_then(|inner| inner.strip_suffix(')'))
-            {
-                redacted_buffer.push_str("Some(");
-                redact.redact_partial(inner, redacted_buffer);
-                redacted_buffer.push(')');
-            } else {
-                // This should never happen, but just in case...
-                redact.redact_full(debug_buffer, redacted_buffer);
-            }
-            return Ok(());
-        }
+    if redact.partial {
+        redact.redact_partial(&debug_formatted, &mut redacted);
+    } else {
+        redact.redact_full(&debug_formatted, &mut redacted);
+    }
 
-        if redact.partial {
-            redact.redact_partial(debug_buffer, redacted_buffer);
-        } else {
-            redact.redact_full(debug_buffer, redacted_buffer);
-        }
-
-        Ok(())
-    })
+    Ok(DisplayDebug(redacted))
 }
