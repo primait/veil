dd9c3eed9a7a3f785632280333725ae92fa4f996
mae.kasza <mae.kasza@prima.it>
2022-09-22 15:52:11 +0200

    Move tests that disable redaction into a separate crate
    This is neccessary because rust runs all tests in the same process,
    leading to tests failing randomly, because of other tests modifying the
    global state. There is some proposal on the rust compiler-team github
    repo but until that gets stabilized we have to use this hack instead
    https://github.com/rust-lang/compiler-team/issues/508

---
+++ veil-tests/disable-redaction-test/src/lib.rs
@@ -0,0 +1,12 @@
+#![cfg_attr(not(test), allow(unused))]
+/// Simple test that ensures veil can actually be disabled
+
+use veil::Redact;
+use veil_tests::{SENSITIVE_DATA, assert_has_sensitive_data};
+#[test]
+fn test_veil_can_be_disabled() {
+    #[derive(Redact)]
+    struct SensitiveWrapper(#[redact] String);
+    veil::set_debug_format(veil::DebugFormat::Plaintext).ok();
+    assert_has_sensitive_data(SensitiveWrapper(SENSITIVE_DATA[0].to_string()));
+}
