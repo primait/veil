26ee7f76d6b4b177de81599a028cd5a55dcc56e7
William Venner <14863743+WilliamVenner@users.noreply.github.com>
2022-12-20 11:35:46 +0000

    Fix redactor tests

--- src/redactor.rs
+++ src/redactor.rs
@@ -66,7 +66,7 @@ impl Redactor {
     /// # Example
     ///
     /// ```rust
-    /// # use veil::RedactorBuilder;
+    /// # use veil::redactor::RedactorBuilder;
     /// let email = "john.doe@prima.it".to_string();
     /// let name = "John Doe".to_string();
     ///
@@ -101,7 +101,7 @@ impl Redactor {
     /// # Example
     ///
     /// ```rust
-    /// # use veil::RedactorBuilder;
+    /// # use veil::redactor::RedactorBuilder;
     /// let mut email = "john.doe@prima.it".to_string();
     /// let mut name = "John Doe".to_string();
     ///
@@ -130,7 +130,7 @@ impl Redactor {
     /// # Example
     ///
     /// ```rust
-    /// # use veil::RedactorBuilder;
+    /// # use veil::redactor::RedactorBuilder;
     /// let email = "john.doe@prima.it".to_string();
     /// let name = "John Doe".to_string();
     ///
