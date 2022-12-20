ef7152170f07d36451aeb4e6891ccb01e9723128
William Venner <14863743+WilliamVenner@users.noreply.github.com>
2022-12-20 10:56:01 +0000

    Remove `Display` requirement for `Redactable` trait

--- src/redactable.rs
+++ src/redactable.rs
@@ -1,9 +1,7 @@
 /// Types that are sensitive data or PII (Personally Identifiable Information) and can be redact-formatted.
-///
-/// The type must have an implementation of [`Display`](std::fmt::Display) so that it can also be formatted in plain text without redaction.
-///
-/// This trait can be derived using the [`Redactable`](derive.Redactable.html) macro.
-pub trait Redactable: std::fmt::Display {
+//
+/// This trait can be manually implemented or derived using the [`Redactable`](derive.Redactable.html) macro.
+pub trait Redactable {
     /// Returns this value formatted as a string with all PII/sensitive data redacted.
     fn redact(&self) -> String {
         let mut buffer = String::new();
