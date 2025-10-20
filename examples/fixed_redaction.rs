dab98d4b0684f6811bebf7ba6613ea6d6f7625fc
Cristiano Piemontese <cristiano.piemontese@prima.it>
2025-10-20 12:19:11 +0200

    docs: add example for fixed redaction

---
+++ examples/fixed_redaction.rs
@@ -0,0 +1,17 @@
+use veil::Redact;
+
+#[derive(Redact)]
+struct AuthInfo {
+    login: String,
+    #[redact(fixed = 3, with = 'X')]
+    password: String,
+}
+
+fn main() {
+    let info = AuthInfo {
+        login: "name".to_string(),
+        password: "/Jb@`?f.?%!hj2".to_string(),
+    };
+
+    println!("{info:#?}");
+}
